150647435a99eaef72341001c29c60afc81740c8
Victor <filyk1010@gmail.com>
2021-10-11 16:49:45 +0300

    Testing navigation menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-
-})
+  const headerButton = $("#header-button");
+  const headerNav = $("#header-nav");
+  headerButton.on("click", () => {
+    const displayProp = headerNav.css("display");
+    if (displayProp !== "none") {
+      headerNav.css("display", "none");
+    } else {
+      headerNav.css("display", "flex");
+    }
+  });
+});
