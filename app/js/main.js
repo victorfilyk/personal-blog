5d6033fb0c53090343b7fe804542d6dba29606b6
Victor <filyk1010@gmail.com>
2021-10-11 17:41:43 +0300

    Testing nav on different devices

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,13 @@
 $(function () {
+  // header nav handler for mobile
   const headerButton = $("#header-button");
   const headerNav = $("#header-nav");
+
   headerButton.on("click", () => {
-    const displayProp = headerNav.css("top");
-    const displayProp2 = headerNav.css("display");
-    console.log(displayProp2)
-    if (displayProp2 === "flex") {
-      if (displayProp === "0px") {
-        headerNav.css("top", "-500px");
-      } else {
-        headerNav.css("top", "0px");
-      }
+    if (headerNav.css("top") === "0px") {
+      headerNav.css("top", "-500px");
     } else {
-        headerNav.css("top", "0px");
+      headerNav.css("top", "0px");
     }
   });
 });
