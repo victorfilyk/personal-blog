596051c0d7bed5bbd9d2778754d0a5a8d6bac290
Victor <filyk1010@gmail.com>
2021-10-11 17:12:05 +0300

    Testing header nav on mobile

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,17 @@ $(function () {
   const headerButton = $("#header-button");
   const headerNav = $("#header-nav");
   headerButton.on("click", () => {
-    const displayProp = headerNav.css("display");
-    if (displayProp !== "none") {
-      headerNav.css("display", "none");
+    const displayProp = headerNav.css("top");
+    const displayProp2 = headerNav.css("display");
+    console.log(displayProp2)
+    if (displayProp2 === "flex") {
+      if (displayProp === "0px") {
+        headerNav.css("top", "-500px");
+      } else {
+        headerNav.css("top", "0px");
+      }
     } else {
-      headerNav.css("display", "flex");
+        headerNav.css("top", "0px");
     }
   });
 });
