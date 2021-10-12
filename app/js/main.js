1b2aa39f5bf6b794820c29ed45e7f05479dcdd3c
Victor <filyk1010@gmail.com>
2021-10-12 17:24:00 +0300

    Test

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,14 @@ $(function () {
   const headerNav = $("#header-nav");
 
   headerButton.on("click", () => {
-    if (headerNav.css("top") === "0px") {
-      headerNav.css("top", "-500px");
+    if (headerNav.css("right") === "0px") {
+      headerNav.css("right", "-100%");
+      headerNav.css("box-shadow", "none");
+      $(document.body).css("overflow-y", "visible");
     } else {
-      headerNav.css("top", "0px");
+      headerNav.css("right", "0px");
+      headerNav.css("box-shadow", "0 0 4em 0 #000000b3");
+      $(document.body).css("overflow-y", "hidden");
     }
   });
 });
